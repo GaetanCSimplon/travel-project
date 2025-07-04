e1b402b0cffb74575a22ecf725113b91f8a10a14
GaetanCSimplon <gcourtieupro@gmail.com>
2025-07-04 12:33:07 +0200

    Exo 4 avancement 4.2

--- clean_data.py
+++ clean_data.py
@@ -1,17 +1,21 @@
 import pandas as pd
 
-clean_data = pd.read_csv('travel_data.csv')
-# Remplacement des valeurs manquantes
-clean_data.fillna('unknown')
+# Création d'une fonction 
+def load_and_clean_data(filename, new_filename):
+    # Récupération du csv
+    clean_data = pd.read_csv('travel_data.csv')
+    # Remplacement des valeurs manquantes
+    clean_data.fillna('unknown')
 
-# Présence d'une valeur négative dans les photos
-# Utilisation de la fonction where() pour localiser et changer les valeurs négatives et les mettre à 0
-# L'alternative à cette méthode serait d'interpréter les valeurs négatives comme une erreur de saisie et
-# de ramener ces valeurs en positif
-clean_data['photos'] = clean_data['photos'].where(clean_data['photos'] > 0, 0)
+    # Présence d'une valeur négative dans les photos
+    # Utilisation de la fonction where() pour localiser et changer les valeurs négatives et les mettre à 0
+    # L'alternative à cette méthode serait d'interpréter les valeurs négatives comme une erreur de saisie et
+    # de ramener ces valeurs en positif
+    clean_data['photos'] = clean_data['photos'].where(clean_data['photos'] > 0, 0)
 
-# Conversion au format datetime
-clean_data['date'] = pd.to_datetime(clean_data['date'])
+    # Conversion au format datetime
+    clean_data['date'] = pd.to_datetime(clean_data['date'])
 
-clean_data.to_csv('clean_data_exo4.csv', index=False)
+    clean_data.to_csv(new_filename, index=False)
 
+load_and_clean_data('clean_travel_data.csv', 'clean_travel_data_clear.csv')
--- print_stat.py
+++ print_stat.py
@@ -1,8 +1,27 @@
 import pandas as pd
-import plotly.express as px
 
-df_trips = pd.read_csv('clean_data_exo4.csv')
+def print_statistics(filename):
+    df_trips = pd.read_csv(filename)
+    # Affichage distribution valeur météo et humeur
+    show_weather_mood = df_trips[['weather', 'mood']]
+    print(show_weather_mood)
+    # Affichage moyenne des photos
+    show_average_photo = df_trips['photos'].mean()
+    print('Le nombre de photo moyen est de ', show_average_photo)
+    # Affichage médiane
+    show_mediane_photo = df_trips['photos'].median()
+    print('La médiane est de ', show_mediane_photo)
+    # Affichage min
+    show_min_photo = df_trips['photos'].min()
+    print('Le nombre minimum de photo prise est de ', show_min_photo)
+    # Affichage max
+    show_max_photo = df_trips['photos'].max()
+    print('Le nombre maximum de photo prise est de ', show_max_photo)
+    # Affichage du nombre de valeur manquante
+    show_missing = df_trips.isna().sum()
+    print(show_missing)
+    # Affichage du nombre de valeur num erronée
+    show_error = df_trips['photos'].fillna
+    print(show_error)
 
-# def print_statistics():
-
-    
+print_statistics('clean_data_exo4.csv')
